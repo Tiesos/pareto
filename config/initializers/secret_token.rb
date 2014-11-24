ea6aaf65d2aef4ebdd016388b9a2682f49fbda44
Danilo Salaz <danilo_salaz@hotmail.es>
2014-11-24 12:35:34 -0500

    Creacion pareto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pareto::Application.config.secret_key_base = 'a0ab971a64f8c6675b8ea9e5c1a0d09d3f5a1ed732b22fff22d328728c914937e044ba20e23015c018e91eb321dfdc6e4ece6d2775b1daa79d30c1e7238f107e'
